435b6730ca46a7fd8517ba56329683e2908a176e
travelandcode <ryonwalkerjnr2001@gmail.com>
2024-07-13 12:13:56 -0500

    feat - created remaining models

---
+++ AssignmentScheduler/Models/Profile.cs
@@ -0,0 +1,17 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssignmentScheduler.Models
+{
+    public class Profile
+    {
+        [BsonId]
+        public ObjectId Id { get; set; }
+        public string Name { get; set; }
+    }
+}
