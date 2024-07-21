0b5987333bdf822489a5d31a24097b1715b57ff6
travelandcode <ryonwalkerjnr2001@gmail.com>
2024-07-21 11:21:44 -0500

    feat - created email service

---
+++ AssignmentScheduler/Interfaces/IEmailService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssignmentScheduler.Interfaces
+{
+    public interface IEmailService
+    {
+        Task SendEmail(string[] emails, string subject, List<byte[]> attachments);
+    }
+}
