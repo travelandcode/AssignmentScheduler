b99309dfaec60812acde4b2c87846715159f4a01
travelandcode <ryonwalkerjnr2001@gmail.com>
2024-08-02 22:18:36 -0500

    feat - added functionality to get and read excel file from s3 bucket

---
+++ AssignmentScheduler/Interfaces/IAwsS3.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssignmentScheduler.Interfaces
+{
+    public interface IAwsS3
+    {
+        Task UploadFileAsync(byte[] fileBytes, string fileName, string contentType);
+
+        Task<String> GetFile(string month);
+    }
+}
--- AssignmentScheduler/Worker.cs
+++ AssignmentScheduler/Worker.cs
@@ -1,4 +1,5 @@
 using AssignmentScheduler.Interfaces;
+using AssignmentScheduler.Repositories;
 using Microsoft.Extensions.Options;
 using System.Globalization;
 
@@ -6,12 +7,11 @@ namespace AssignmentScheduler
 {
     public class Worker : BackgroundService
     {
-        private readonly IServiceProvider _serviceProvider;
+        private readonly IServiceScopeFactory _serviceScopeFactory;
         private Timer _timer;
-
-        public Worker(IServiceProvider serviceProvider)
+        public Worker(IServiceScopeFactory serviceScopeFactory)
         {
-            _serviceProvider = serviceProvider;
+            _serviceScopeFactory = serviceScopeFactory;
         }
 
         protected override Task ExecuteAsync(CancellationToken stoppingToken)
@@ -25,29 +25,38 @@ namespace AssignmentScheduler
             SendAssignmentSchedule();
         }
 
+
+
         private async void SendAssignmentSchedule()
         {
-            if (DateTime.Now.Day == 21)
+            if (DateTime.Now.Day == 2)
             {
-                using (var scope = _serviceProvider.CreateScope())
+                using (var scope = _serviceScopeFactory.CreateScope())
                 {
                     var excelGenerator = scope.ServiceProvider.GetRequiredService<IAssignmentExcelGenerator>();
                     var emailService = scope.ServiceProvider.GetRequiredService<IEmailService>();
+                    var s3Service = scope.ServiceProvider.GetRequiredService<IAwsS3>();
+                    var monthRepository = scope.ServiceProvider.GetRequiredService<IMonthRepository>();
                     // Calculate the start and end dates for the previous month
-                    DateTime now = DateTime.Now;
+                    DateTime now = DateTime.Now; ;
                     DateTime previousMonth = new DateTime(now.Year, now.Month, 1).AddMonths(-1);
                     string previousMonthFormatted = previousMonth.ToString("MMMM yyyy", CultureInfo.InvariantCulture);
+                    var nextMonthInPatwa = await monthRepository.GetNextMonth();
+                    var prevMonthInPatwa = await monthRepository.GetCurrentMonth();
+
+                   var lastManUsed = await s3Service.GetFile($"{prevMonthInPatwa} {now.Year}");
+
 
                     //Generate Excel Files using stored procedure
-                    List<byte[]> excelAttachments = new List<byte[]> { await excelGenerator.GenerateAssignmentSchedule() };
+                    List<byte[]> excelAttachments = new List<byte[]> { await excelGenerator.GenerateAssignmentSchedule(lastManUsed) };
                     string[] recipientEmails = { "ryonwalkerjnr2001@gmail.com" }; // recipient emails
-                    string subject = "Papiin Asainment Skedyuul";
+                    string subject = $"Papiin Asainment Skedyuul ({nextMonthInPatwa} {now.Year})";
 
-                    // Send email with the attachments
-                   
+                    //Upload Excel File to S3 Bucket
+                    await s3Service.UploadFileAsync(excelAttachments[0], $"Papiin Asainment Skedyuul ({nextMonthInPatwa} {now.Year})", "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet");
 
                     // Send email with attachments
-                    await emailService.SendEmail(recipientEmails, subject, excelAttachments);
+                    await emailService.SendEmail(recipientEmails, subject, excelAttachments,$"{nextMonthInPatwa} {now.Year}");
                 }
             }
         }
